21b48e985d488f8a72c26733b6a087018c151ce2
Esteban Morales <eamu11@hotmail.com>
2019-09-24 15:30:27 -0600

    Atributos de mi clase conector

--- Proyecto/src/Connector.java
+++ Proyecto/src/Connector.java
@@ -1,2 +1,17 @@
-public class Connector {
-}
+import java.awt.*;
+
+abstract class Connector { //clase de manera "general" para mis outputs e inputs
+
+    boolean value; //Valores de inputs y outputs, no de las compuertas
+    boolean hasValue = false;
+    boolean isInput;
+    boolean isOutput;
+    int id;
+    Component component;
+    Mi_Lista<Connection> connections = new Mi_Lista<>();
+    int maxConnections;
+    int x, y; //coordenadas
+    int w = 10, h = 10; //dimensiones
+    Shape shape;
+    Color color;
+
