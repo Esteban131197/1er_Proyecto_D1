199604480790f698ad81c64e921d155e0aaf63d9
Esteban Morales <eamu11@hotmail.com>
2019-09-24 17:15:00 -0600

    Modificacion  y metodos a clase StarPoint

--- Proyecto/src/StarTPoint.java
+++ Proyecto/src/StarTPoint.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
 public class StartPoint extends Component {
 
 
@@ -5,3 +9,42 @@ public class StartPoint extends Component {
         super(ID, "Start", x, y);
         type = "Start";
         value = 0;
+        this.setText(Integer.toString(value)); /
+        //formato:
+        setVerticalAlignment(SwingConstants.CENTER);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setBounds(x, y, 70, 30);
+        setBorder(new LineBorder(Color.black, 1));
+        Output bye = new Output(Main.currentConnectorID, this, getWidth(), getHeight() /2);
+        Input hi = new Input(Main.currentConnectorID, this, 0, getHeight() /2);
+        hi.hasValue = true;
+        hi.maxConnections = 0;
+
+
+    }
+    boolean isAvailable() {
+        return false;
+    }
+    public boolean operation(Mi_Lista<Boolean> args) { /
+
+        return outputs.get(0).value;
+    }
+    public void toggle() {
+        System.out.println("toggling");
+        value +=1;
+        value %= 2;
+        if (value == 0) {
+            outputs.get(0).value = false;
+        }
+        else {
+            outputs.get(0).value = true;
+        }
+
+
+        this.setText(Integer.toString(value));
+
+    }
+
+
+
+}
