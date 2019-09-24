cb1095d4546638c3d213566b670983f22c7d3691
Esteban Morales <eamu11@hotmail.com>
2019-09-24 14:39:55 -0600

    Modificacion a mi lista enlazada

--- Proyecto/src/Mi_Lista.java
+++ Proyecto/src/Mi_Lista.java
@@ -1,2 +1,18 @@
 public class Mi_Lista {
+
+    public Mi_Lista<Object> connections;
+    private Nodo cabeza = null;
+    private int longitud = 0;
+
+
+    private class Nodo{
+        public Component component;
+        public Nodo siguiente = null;
+
+        public Nodo(Component component){
+            this.component = component;
+
+        }
+    }
+
 }
