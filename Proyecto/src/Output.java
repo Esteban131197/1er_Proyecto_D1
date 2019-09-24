9a0d4cd84ec7c3fe69ac21e8dd856d77a17d3203
Esteban Morales <eamu11@hotmail.com>
2019-09-24 17:25:31 -0600

    Declaracion de las salidas en clase output

--- Proyecto/src/Output.java
+++ Proyecto/src/Output.java
@@ -1,5 +1,22 @@
-public class Output extends Connector {//Output class - type of connector
-    //Output-specific code
+import java.awt.*;
+
+public class Output extends Connector {
 
     Mi_Lista<Input> inputsReceivingThis = new Mi_Lista<>();
+
+    Output(int ID, Component owner, int x, int y) {
+        super(ID, owner, x, y);
+        isOutput = true;
+        owner.addOutput(this);
+        maxConnections = 100; // maxima cant de outputs
+        Polygon p = new Polygon();
+        p.addPoint(x - w, y - h / 2);
+        p.addPoint(x, y - h/2);
+        p.addPoint(x, y + h/2);
+        p.addPoint(x - w, y + h / 2);
+
+        shape = p;
+        color = Color.red;
+    }
+}
 
