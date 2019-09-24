744640d6ca91660794915d0720ba6ea42dcf87b9
Esteban Morales <eamu11@hotmail.com>
2019-09-24 14:52:37 -0600

    Los atributos de mi clase component

--- Proyecto/src/Component.java
+++ Proyecto/src/Component.java
@@ -1,2 +1,16 @@
-public class Component {
+//En esta clase hago metodos y eventos necesarios para que mi interfaz use la lista
+//Esta clase sera la que le herede a mis compuertas, mi simulacion y mi resultado de simulacion
+
+import javax.swing.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+abstract class Component extends JLabel implements MouseListener, MouseMotionListener {
+
+    int numberConnected = 0;
+    int id;
+    String type;
+
+
+
 }
