524ad169b79e1af96246d73d2898f71d5a47dcc0
Esteban Morales <eamu11@hotmail.com>
2019-09-24 14:56:16 -0600

    Llamando a mi lista en mi clase component

--- Proyecto/src/Component.java
+++ Proyecto/src/Component.java
@@ -11,6 +11,21 @@ abstract class Component extends JLabel implements MouseListener, MouseMotionLis
     int id;
     String type;
 
+    public void toggle() { //Is overwritten only in the StartPoint class - go there if you dare...
+    }
+    public boolean operation(Mi_Lista<Boolean> args) { //overwritten by subclasses
+        return false;
+    }
+
+    public Mi_Lista<Boolean> userOperation(Mi_Lista<Boolean> args) { //overwritten by subclasses
+        return (new Mi_Lista());
+    }
+
+    int value = 0; //Valor por defecto de una entrada
+    Mi_Lista<Input> inputs = new Mi_Lista<>(); //lista de inputs
+    Mi_Lista<Input> toggles = new Mi_Lista<>(); // list lo que contiene un input
+    Mi_Lista<Output> outputs = new Mi_Lista<>();//lista de outputs
+
 
 
 }
