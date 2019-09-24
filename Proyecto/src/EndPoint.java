584b0c6b61bc63ba0967e39afa119a3c6b553c56
Esteban Morales <eamu11@hotmail.com>
2019-09-24 15:41:00 -0600

    Manejo de intefax en mi clase EndPoint

--- Proyecto/src/EndPoint.java
+++ Proyecto/src/EndPoint.java
@@ -1,2 +1,20 @@
-public class EndPoint {
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class EndPoint extends Component {
+    EndPoint(int ID, int x, int y) {
+        super(ID, "End", x, y);
+        type = "End";
+
+        setVerticalAlignment(SwingConstants.CENTER);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setBounds(x, y, 70, 30);
+        setBorder(new LineBorder(Color.black, 1));
+
+        new Input(Main.currentConnectorID, this, 0, getHeight() / 2);
+
+    }
+
 }
+
