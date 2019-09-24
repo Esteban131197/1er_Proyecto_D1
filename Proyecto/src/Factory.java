ca1c41780bbb8f2e0e6176d12be73c0b61a46f59
Esteban Morales <eamu11@hotmail.com>
2019-09-24 15:45:59 -0600

    Modificacion a mi clase Factory

--- Proyecto/src/Factory.java
+++ Proyecto/src/Factory.java
@@ -1,2 +1,26 @@
+import java.awt.event.MouseEvent;
+
 public class Factory {
+    public static Component ComponentFactory(TypeComponent type, MouseEvent e) {
+        if (type == TypeComponent.AND) {
+            return new AndGate(Main.currentComponentID, e.getX(), e.getY());
+        }
+        if (type == TypeComponent.OR) {
+            return new OrGate(Main.currentComponentID, e.getX(), e.getY());
+        }
+        if (type == TypeComponent.NAND) {
+            return new NandGate(Main.currentComponentID, e.getX(), e.getY());
+        }
+        if (type == TypeComponent.NOR) {
+            return new NorGate(Main.currentComponentID, e.getX(), e.getY());
+        }
+        if (type == TypeComponent.NOT) {
+            return new NotGate(Main.currentComponentID, e.getX(), e.getY());
+        }
+        if (type == TypeComponent.XNOR) {
+            return new XnorGate(Main.currentComponentID, e.getX(), e.getY());
+        } else {
+            return new XorGate(Main.currentComponentID, e.getX(), e.getY());
+        }
+    }
 }
